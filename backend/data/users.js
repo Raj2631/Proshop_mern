5f301e62886d9b7b5e7bb341c4fbb70a827acfd7
Rakshit Pathak <rajatpathak435@gmail.com>
2021-01-16 21:31:06 +0530

    Custom Error Handler

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,21 +1,21 @@
-import bcrypt from "bcryptjs";
+import bcrypt from 'bcryptjs'
 
 const users = [
   {
-    name: "Admin User",
-    email: "admin@example.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
   },
   {
-    name: "User1",
-    email: "user1@example.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'User1',
+    email: 'user1@example.com',
+    password: bcrypt.hashSync('123456', 10),
   },
   {
-    name: "User2",
-    email: "user2@example.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'User2',
+    email: 'user2@example.com',
+    password: bcrypt.hashSync('123456', 10),
   },
-];
+]
 
-export default users;
+export default users
--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,31 @@
-import express from "express";
-import colors from "colors";
-import dotenv from "dotenv";
-import products from "./data/products.js";
-import connectDB from "./config/db.js";
+import express from 'express'
+import colors from 'colors'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
-dotenv.config();
+dotenv.config()
 
-connectDB();
+connectDB()
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes)
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound)
 
-const PORT = process.env.PORT || 5000;
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
-);
+)
